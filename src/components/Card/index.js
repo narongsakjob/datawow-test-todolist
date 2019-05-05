ea37a7e86eecae09b41d7a682fefca17bd13a01a
narongsakjob <narongsak.chobsri@gmail.com>
2019-05-05 21:27:26 +0700

    Add card style

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,18 +3,30 @@ import { SortableElement, sortableHandle } from 'react-sortable-hoc'
 
 import { taskContext } from '../../context'
 import menu from '../../images/menu.svg'
+import edit from '../../images/edit.svg'
+import trash from '../../images/trash.svg'
 
-import { List } from './styles'
+import {
+  List,
+  Menu,
+  Item,
+  FinishButton,
+  Icon
+} from './styles'
 
-const DragHandle = sortableHandle(() => <img src={menu} alt="menu" />)
+const DragHandle = sortableHandle(() => <Menu src={menu} alt='menu' />)
 
 const Card = ({ value, index }) => {
   const { removeTask } = useContext(taskContext)
   return (
     <List>
-      <DragHandle />
-      {value}
-      <button onClick={() => removeTask(index)}>Remove Task</button>
+      <Item column={1}><DragHandle /></Item>
+      <Item column={8}>{value}</Item>
+      <Item column={3} align={'flex-end'}>
+        <Icon src={edit} alt='edit' />
+        <Icon src={trash} alt='trash' onClick={() => removeTask(index)} />
+        <FinishButton onClick={() => console.log('Finish')}>Finish</FinishButton>
+      </Item>
     </List>
   )
 }
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,9 +1,57 @@
+import React from 'react'
 import styled from 'styled-components'
 
 export const List = styled.li`
   list-style: none;
   background-color: #fff;
-  display: block;
+  display: flex;
+  flex-wrap: wrap;
   margin-bottom: 8px;
   border-radius: 3px;
+  border: 1px solid #dfe1e6;
+  padding: 5px;
+  box-shadow: 0 1px 0 rgba(9, 45, 66, 0.25);
+`
+
+export const Menu = styled.img`
+  cursor: pointer;
+`
+
+const ItemContainer = styled.div`
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+  word-break: break-all;
+  align-items: center;
+  display: flex;
+  padding: 0 10px;
+`
+
+export const Item = ({ children, column, align='flex-start' }) => (
+  <ItemContainer style= {{ flex: `0 0 ${column/12 * 100}%`, maxWidth: `${column/12*100}%`, justifyContent: align }}>
+    {children}
+  </ItemContainer>
+)
+
+export const Icon = styled.img`
+  margin-right: 10px;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(0.8);
+    cursor: pointer;
+  }
+`
+
+export const FinishButton = styled.button`
+  background-color: #000;
+  color: #fff;
+  border-radius: 6px;
+  padding: 8px;
+  border: 0;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(0.8);
+    cursor: pointer;
+  }
 `
